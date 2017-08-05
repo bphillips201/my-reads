0c4b500195660272339fab6ce80e4e0843622c8c
Brian Phillips <brian.phillips@mindbodyonline.com>
2017-08-04 21:28:10 -0700

    showing selected shelves in dropdown

--- src/components/ListBooks.js
+++ src/components/ListBooks.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 class ListBooks extends Component {
+
   render() {
     return (
       <div className="bookshelf-books">
@@ -12,7 +13,7 @@ class ListBooks extends Component {
                   <div className="book-top">
                     <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
                     <div className="book-shelf-changer">
-                      <select>
+                      <select value={book.shelf}>
                         <option value="none" disabled>Move to...</option>
                         <option value="currentlyReading">Currently Reading</option>
                         <option value="wantToRead">Want to Read</option>
