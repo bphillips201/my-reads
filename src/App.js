25d25989d2a6bd03687d5348ab6c21de30b46237
Brian Phillips <brian.phillips@mindbodyonline.com>
2017-08-06 13:42:31 -0700

    added SearchBooks component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import * as BooksAPI from './utils/BooksAPI'
-import './App.css'
-import ListShelves from './components/ListShelves';
 import { Route, Link } from 'react-router-dom';
+import * as BooksAPI from './utils/BooksAPI'
+import ListShelves from './components/ListShelves';
+import SearchBooks from './components/SearchBooks';
+import './App.css'
 
 class BooksApp extends React.Component {
   state = {
@@ -19,32 +20,17 @@ class BooksApp extends React.Component {
     this.setState(() => {
       book.shelf = shelf;
     })
+
+    BooksAPI.update(book, shelf);
   }
 
   render() {
     return (
       <div className="app">
         <Route path="/search" render={() => (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <Link to="/" className="close-search">Close</Link>
-              <div className="search-books-input-wrapper">
-                {/* 
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-                  
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-                
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
+          <SearchBooks
+            bookData={this.state.books}
+          />
         )}/>
         <Route exact path="/" render={() => (
           <div className="list-books">
