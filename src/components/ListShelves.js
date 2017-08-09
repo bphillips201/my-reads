f4e5ed19ff6d1837751177420520ffe3560c6164
Brian Phillips <brian.phillips@mindbodyonline.com>
2017-08-09 09:38:52 -0700

    create stateless functional components

--- src/components/ListShelves.js
+++ src/components/ListShelves.js
@@ -1,35 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Book from './Book';
 
-class ListShelves extends Component {
-  
-  render() {
+const ListShelves = (props) => {
+  return (
+    <div>
+      {props.shelves.map((bookshelf, index) => (
+        <div key={index} className="bookshelf">
+          <h2 className="bookshelf-title">{bookshelf.label}</h2>
 
-    return (
-      <div>
-        {this.props.shelves.map((bookshelf, index) => (
-          <div key={index} className="bookshelf">
-            <h2 className="bookshelf-title">{bookshelf.label}</h2>
-
-            <div className="bookshelf-books">
-              <ol className="books-grid">
-                {this.props.library
-                  .filter(b => (b.shelf === bookshelf.id))
-                  .map((book) => (
-                  <li key={book.id}>
-                    <Book
-                      book={book}
-                      updateShelf={this.props.updateShelf}
-                    />
-                  </li>
-                ))}
-              </ol>
-            </div>
+          <div className="bookshelf-books">
+            <ol className="books-grid">
+              {props.library
+                .filter(b => (b.shelf === bookshelf.id))
+                .map((book) => (
+                <li key={book.id}>
+                  <Book
+                    book={book}
+                    updateShelf={props.updateShelf}
+                  />
+                </li>
+              ))}
+            </ol>
           </div>
-        ))}
-      </div>
-    )
-  }
+        </div>
+      ))}
+    </div>
+  )
 }
 
 export default ListShelves;
